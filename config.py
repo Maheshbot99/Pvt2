02fa22fa5299b6cfd40e0e1eb6c61b152b75ce24
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-08-01 07:19:18 +0530

    Rename 💥

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
+ADMIN = int(environ.get("ADMINS", ""))          
 CAPTION = environ.get("CAPTION", "")
 
 # for thumbnail 
