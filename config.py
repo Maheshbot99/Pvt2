4c3e4473617b936e8fc0a8770d0be6c2faf9a083
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-08-01 07:18:35 +0530

    Rename 💥

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 ADMIN = int(environ.get("ADMIN", ""))          
 CAPTION = environ.get("CAPTION", "")
 
-# for thumbnail
+# for thumbnail 
 DOWNLOAD_LOCATION = "./DOWNLOADS"
 
