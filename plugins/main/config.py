12c5343fb38ea79863403d875a90ef3ade78065b
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-08-01 07:45:02 +0530

    Update config.py

--- plugins/main/config.py
+++ plugins/main/config.py
@@ -3,7 +3,7 @@ from os import environ
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
+ADMINS = int(environ.get("ADMINS", ""))          
 CAPTION = environ.get("CAPTION", "")
 
 # for thumbnail ( back end is MrMKN brain 😉)
