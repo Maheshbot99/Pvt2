c3a99d243a8e5cdac6dc8499f9786d3b7be2db17
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-08-01 08:12:46 +0530

    Update kingfy.py

--- kingfy.py
+++ kingfy.py
@@ -3,7 +3,7 @@ from os import environ
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMINS = int(environ.get("ADMINS", ""))          
+ADMIN = int(environ.get("ADMIN", ""))          
 CAPTION = environ.get("CAPTION", "")
 
 # for thumbnail ( back end is MrMKN brain 😉)
