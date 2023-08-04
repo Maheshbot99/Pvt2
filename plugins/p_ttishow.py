f4b3fa4d4a8afcf556aeb78cce968ab9d1dacd69
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-08-04 19:56:11 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -16,7 +16,7 @@ from datetime import datetime
 from utils import get_size, temp, get_settings, get_readable_time
 import time
 
-"""-----------------------------------------https://t.me/TG_UPDATES1/300 --------------------------------------"""
+"""-----------------------------------------https://t.me/NewCinemaClub --------------------------------------"""
 
 @Client.on_message(filters.new_chat_members & filters.group)
 async def save_group(bot, message):
@@ -45,7 +45,7 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-                    InlineKeyboardButton('📣 Uᴘᴅᴀᴛᴇs', url='https://t.me/TG_UPDATES1'),
+                    InlineKeyboardButton('📣 Uᴘᴅᴀᴛᴇs', url='https://t.me/NewCinemaClub'),
                     InlineKeyboardButton('❓ Hᴇʟᴘ', url=f"https://t.me/{temp.U_NAME}?start=help")
                   ]]
         reply_markup=InlineKeyboardMarkup(buttons)
@@ -67,7 +67,7 @@ async def save_group(bot, message):
                                                  caption=(script.MELCOW_ENG.format(u.mention, message.chat.title)),
                                                  reply_markup=InlineKeyboardMarkup(
                                                                          [[
-                                                                             InlineKeyboardButton('🍁 sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ​ 🍁', url='https://t.me/TG_UPDATES1')
+                                                                             InlineKeyboardButton('🍁 sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ​ 🍁', url='https://t.me/NewCinemaClub')
                                                                         
                                                                         ]]
                                                  ),
