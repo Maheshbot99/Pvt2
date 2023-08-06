d033b321a4116870560046b962a08ffba887c823
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-08-06 18:54:35 +0530

    Update font.py

--- plugins/local_plugins/font.py
+++ plugins/local_plugins/font.py
@@ -35,7 +35,7 @@ async def style_buttons(c, m, cb=False):
         InlineKeyboardButton('H̆̈ă̈p̆̈p̆̈y̆̈', callback_data='style+happy'),
         InlineKeyboardButton('S̑̈ȃ̈d̑̈', callback_data='style+sad'),
         ],[
-        InlineKeyboardButton('ɴᴇxᴛ ➡️', callback_data="nxt")
+        InlineKeyboardButton('ɴᴇxᴛ ➡️', callback_data="nxt"),
         InlineKeyboardButton('ᴄʟᴏsᴇ ⊝', callback_data='close_data')
     ]]
     if not cb:
