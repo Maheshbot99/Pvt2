1f656de3cd6f1cea55fa2837f64cc836d6c1c021
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-08-11 08:53:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -280,8 +280,7 @@ async def next_page(bot, query):
                     ],
                 )
     btn.insert(0, [
-        InlineKeyboardButton(f'🔍 {search} 🔎', 'reqst1')],
-       [InlineKeyboardButton("ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ​", url="https://t.me/NewCinemaClub/8")
+        InlineKeyboardButton(f'🔍 {search} 🔎', url=f't.me/NewCinemaClub/8')
     ])
     try:
         await query.edit_message_reply_markup(
